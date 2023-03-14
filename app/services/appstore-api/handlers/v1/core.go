0f8bd73aadf07bc0d343dc94e223b85da625c638
Joshua Seals <jseals@renci.org>
2023-03-14 13:27:52 -0400

    Adding potential helper to core struct

--- app/services/appstore-api/handlers/v1/core.go
+++ app/services/appstore-api/handlers/v1/core.go
@@ -1,11 +1,34 @@
 package v1
 
-import "go.uber.org/zap"
+import (
+	"encoding/json"
+	"net/http"
 
-// Core can hold middleware, context, and logger, etc.
+	"go.uber.org/zap"
+)
+
+// CoreHandler can hold middleware, context, and logger, etc.
 // This struct will bind the API associated methods
-// found in apps.go and dashboard.go and is instantiated
-// in routes.go
+// found in apps.go, dashboard.go and any helpers needed for
+// standardization of data flow.
 type CoreHandler struct {
 	Log *zap.SugaredLogger
+}
+
+type envelope map[string]any
+
+// Method for maintaining a standard response to the client.
+func (c *CoreHandler) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
